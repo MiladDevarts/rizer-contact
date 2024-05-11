b4772fe926eb8264b30d90847d704c1ae5ab14e2
Milad Mohamadi <MiladDevarts@gmail.com>
2024-05-11 15:15:24 +0400

    feat: add form-title

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navigation from "./components/Navigation";
+import Title from "./components/Title";
 
 function App() {
   return (
     <>
       <main className="w-full h-[100vh]">
         <Navigation />
+        <Title />
       </main>
     </>
   );
--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -4,20 +4,26 @@ import RizerLogo from "../../assets/logo/rizer-white.svg";
 const Navigation = () => {
   return (
     <nav className="flex justify-between items-center max-w-[75rem] mx-auto py-[1.87rem]">
-      <a href="/" className="cursor-pointer">
-        <img src={RizerLogo} alt="" />
-      </a>
-      <div className="flex items-center gap-x-[2rem] px-[1.5rem] py-[1rem] bg-[#DBC8FF]/5 backdrop-blur-md text-[#7B7488] rounded-full border border-[#AA80FF]/10">
-        <a href="#">Home</a>
-        <a href="#">Plans</a>
-        <a href="#">Product</a>
-        <a href="#">Contact</a>
-        <a href="#">About us</a>
+      <div className="w-[20%]">
+        <a href="/" className="cursor-pointer">
+          <img src={RizerLogo} alt="" />
+        </a>
       </div>
-      <div className="flex items-center gap-4 text-[#7B7488] ">
-        <a href="#">Login</a>
-        <div class="btn">
-          <a href="#">Get Started</a>
+      <div className="flex justify-center items-center w-[80%]">
+        <div className="flex items-center gap-x-[2rem] px-[1.5rem] py-[1rem] bg-[#DBC8FF]/5 backdrop-blur-md text-[#7B7488] rounded-full border border-[#AA80FF]/10">
+          <a href="#">Home</a>
+          <a href="#">Plans</a>
+          <a href="#">Product</a>
+          <a href="#">Contact</a>
+          <a href="#">About us</a>
+        </div>
+      </div>
+      <div className="w-[20%] flex justify-end">
+        <div className="flex items-center gap-4 text-[#7B7488] ">
+          <a href="#">Login</a>
+          <div class="btn">
+            <a href="#">Get Started</a>
+          </div>
         </div>
       </div>
     </nav>
