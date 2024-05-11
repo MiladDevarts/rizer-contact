b7a12ee5a1af5a5501ee38c1056737f37cff07b4
Milad Mohamadi <MiladDevarts@gmail.com>
2024-05-11 17:07:18 +0400

    feat: add active item on menu

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -10,18 +10,18 @@ const Navigation = () => {
         </a>
       </div>
       <div className="flex justify-center items-center w-[80%]">
-        <div className="flex items-center gap-x-[2rem] px-[1.5rem] py-[1rem] bg-[#DBC8FF]/5 backdrop-blur-md text-[#7B7488] rounded-full border border-[#AA80FF]/10">
+        <div className="menu">
           <a href="#">Home</a>
           <a href="#">Plans</a>
           <a href="#">Product</a>
-          <a href="#">Contact</a>
+          <a className="active" href="#">Contact</a>
           <a href="#">About us</a>
         </div>
       </div>
       <div className="w-[20%] flex justify-end">
         <div className="flex items-center gap-4 text-[#7B7488] ">
           <a href="#">Login</a>
-          <div class="btn">
+          <div class="btn w-[8.25rem] h-[2.68rem]">
             <a href="#">Get Started</a>
           </div>
         </div>
